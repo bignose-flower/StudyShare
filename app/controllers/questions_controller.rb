8d8b1baadbba72b63e954801aa2a257c5bd70bff
bignose-flower <66852865+bignose-flower@users.noreply.github.com>
2020-07-03 21:22:43 +0900

    create question new and create haml and install ancestry

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,19 @@ class QuestionsController < ApplicationController
   # before_action :authenticate_user?, except: [:index]
   def index
   end
+
+  def new
+    @subject = Subject.new
+    @category_parent_array = ["---"]
+    Subject.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.category
+    end
+  end
+
+  def create
+    if Question.save(question_params)
+      recirect_to 
+    else
+      render :new
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "questions#index"
   resources :questions, only: [:index]
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update] do
+    resources :questions, only: [:new, :create]
+  end
   
 
   if Rails.env.development?
