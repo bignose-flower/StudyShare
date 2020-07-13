84ad165a8df7cf4cab72176fcd2d53aafff78853
bignose-flower <66852865+bignose-flower@users.noreply.github.com>
2020-07-13 10:32:43 +0900

    add push notice boolean to User Model

--- db/migrate/20200713012745_add_notice_to_user.rb
+++ db/migrate/20200713012745_add_notice_to_user.rb
@@ -1,5 +1,5 @@
 class AddNoticeToUser < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :notice, :boolean
+    add_column :users, :notice, :boolean, default: false, null: false
   end
 end
