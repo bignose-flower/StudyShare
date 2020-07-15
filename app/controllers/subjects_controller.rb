507f1899bff58c468d7e4c38f5809d8014b2f004
bignose-flower <66852865+bignose-flower@users.noreply.github.com>
2020-07-15 19:34:41 +0900

    add category search and haml and scss

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,2 +1,30 @@
 class SubjectsController < ApplicationController
+  def index
+    parent_subs = Subject.where(ancestry: nil).where.not(category: "カテゴリーなし")
+    @subjects = []
+    find_category(parent_subs)
+  end
+
+  def search_category
+    binding.pry
+    subjects = Subject.find_by(category: params[:category])
+    @questions = subjects.questions.order(created_at: "DESC").page(params[:page]).per(7)
+    @keyword = params[:category]
+    render template: 'questions/search'
+  end
+
+  private
+  def find_category(lists)
+    lists.each do |subject|
+      hash = {}
+      child_lists = []
+      parent_cat = subject.category
+      subject.children.each do |child|
+        child_cat = child.category
+        child_lists << child_cat
+      end
+      hash[parent_cat] = child_lists
+      @subjects << hash
+    end
+  end
 end
