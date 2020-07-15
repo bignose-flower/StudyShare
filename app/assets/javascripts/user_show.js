5cf39e88863d8e3f48460df90dc4a2f75cce493f
bignose-flower <66852865+bignose-flower@users.noreply.github.com>
2020-07-15 12:55:35 +0900

    add css (makle tabs for alternating questionlists and answerlists)

--- app/assets/javascripts/user_show.js
+++ app/assets/javascripts/user_show.js
@@ -1,3 +1,10 @@
 $(document).on('turbolinks:load', function() {
-  
+  $('.Content__questions').on('click', function(){
+    $('.User-Answers').addClass('hidden');
+    $('.User-Questions').toggleClass('hidden');
+  });
+  $('.Content__answers').on('click', function(){
+    $('.User-Questions').addClass('hidden');
+    $('.User-Answers').toggleClass('hidden');
+  });
 });
