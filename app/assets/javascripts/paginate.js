9429980be6e635526fb00a51d9fb4fff5eed61e3
bignose-flower <66852865+bignose-flower@users.noreply.github.com>
2020-07-16 16:49:36 +0900

    pagination set back to default

--- app/assets/javascripts/paginate.js
+++ app/assets/javascripts/paginate.js
@@ -1,7 +1,6 @@
 $(document).on('turbolinks:load', function() {
   $(function(){
     $('.pagination li a').on('click', function(e){
-      $('html, body').animate({ scrollTop: $('.Header')[0].scrollHeight - 50});
     })
   })
 });
