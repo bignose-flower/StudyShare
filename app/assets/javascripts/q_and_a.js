09d0c3a1eb7ed6e392cff3687b454819e6526b22
bignose-flower <66852865+bignose-flower@users.noreply.github.com>
2020-07-18 00:12:07 +0900

    answer index action style change like line chat box

--- app/assets/javascripts/q_and_a.js
+++ app/assets/javascripts/q_and_a.js
@@ -7,12 +7,12 @@ $(document).on('turbolinks:load', function() {
           image = answer.image
       }
       let html = `<div class="AnswerList">
+                    <div class="AnswerInfo">
+                      <div class="AnswerInfo__time">1分前</div>
+                      <div class="AnswerInfo__user"><img class="UserIcon__icon_style" src="${image}"></div>
+                    </div>
                     <div class="AnswerList__answer">
                       ${answer.answer}
-                      <div class="AnswerInfo">
-                        <div class="AnswerInfo__time">1分前</div>
-                        <div class="AnswerInfo__user"><img class="UserIcon__icon_style" src="${image}"></div>
-                      </div>
                     </div>
                   </div>`
       return html;
