e8c7afe814be6c5ad9134135d678d8816de83abb
bignose-flower <66852865+bignose-flower@users.noreply.github.com>
2020-08-13 18:15:36 +0900

    Update mail.rb

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,10 +1,13 @@
+name = Rails.application.credentials.user_name
+pass = Rails.application.credentials.password
+
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   address: "smtp.gmail.com",
   domain: "gmail.com",
   port: 587,
-  user_name: Rails.application.credentials.user_name,
-  password: Rails.application.credentials.password,
+  user_name: name,
+  password: pass,
   authentication: 'login',
   enable_starttls_auto: true
 }
