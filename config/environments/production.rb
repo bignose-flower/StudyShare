1d0e23ab533d5c0c512c09756e7d21f25f8f6443
bignose-flower <66852865+bignose-flower@users.noreply.github.com>
2020-08-13 15:23:54 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.deliver_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     domain: "gmail.com",
