6929942c2a6e8ef4c3cd0cbfcae13df7bc4aa6ac
bignose-flower <66852865+bignose-flower@users.noreply.github.com>
2020-08-13 15:17:30 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,8 +67,8 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     domain: "gmail.com",
     port: 587,
-    user_name: 'sendmailer118835@gmail.com',
-    password: 'takotako12',
+    user_name: Rails.application.credentials.user_name,
+    password: Rails.application.credentials.password,
     authentication: 'login',
     enable_starttls_auto: true
   }
